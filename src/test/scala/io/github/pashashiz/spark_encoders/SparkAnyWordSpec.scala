e7e9ba26234aa6d130ac679a27b157ccca3a3ed1
Peter Podlovics <peter.d.podlovics@gmail.com>
2025-08-19 10:59:53 +0200

    add DBR detection for tests

--- src/test/scala/io/github/pashashiz/spark_encoders/SparkAnyWordSpec.scala
+++ src/test/scala/io/github/pashashiz/spark_encoders/SparkAnyWordSpec.scala
@@ -2,8 +2,17 @@ package io.github.pashashiz.spark_encoders
 
 import org.scalatest.wordspec.AnyWordSpec
 
+object SparkAnyWordSpec {
+  // Auto-detect Databricks Runtime
+  private def isDatabricks: Boolean = {
+    System.getenv("DATABRICKS_RUNTIME_VERSION") != null
+  }
+  
+  private val defaultReuseContext = isDatabricks
+}
+
 abstract class SparkAnyWordSpec(
-    override val reuseContext: Boolean = false,
+    override val reuseContext: Boolean = SparkAnyWordSpec.defaultReuseContext,
     override val parallelism: Int = 2,
     override val shufflePartitions: Int = 4,
     override val sparkUI: Boolean = false,
