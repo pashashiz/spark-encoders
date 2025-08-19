fc80cb209fc3ff3537116aaf0ce4ea0850de5e5c
Peter Podlovics <peter.d.podlovics@gmail.com>
2025-08-19 08:57:17 +0200

    move type defs to InvariantEncoderSpec

--- src/test/scala/io/github/pashashiz/spark_encoders/InvariantEncoderSpec.scala
+++ src/test/scala/io/github/pashashiz/spark_encoders/InvariantEncoderSpec.scala
@@ -1,17 +1,48 @@
 package io.github.pashashiz.spark_encoders
 
-import io.github.pashashiz.spark_encoders.testutils._
-
 class InvariantEncoderSpec extends SparkAnyWordSpec() with TypedEncoderMatchers {
+  import InvariantEncoderSpec._
 
-  "ExternalEncoder" when {
+  "InvariantEncoder" when {
 
     "used with SimpleType" should {
 
-      // TODO: add equality for classes (now eq is based is pointer eq)
-      "support SimpleTypeA" in {
-        new SimpleTypeWithCustomEncoder(SimpleTypeA(true)) should haveTypedEncoder[SimpleTypeWithCustomEncoder]()
+      "work" in {
+        new SimpleTypeWithCustomEncoder(SimpleType(true)) should haveTypedEncoder[SimpleTypeWithCustomEncoder]()
       }
     }
   }
 }
+
+object InvariantEncoderSpec {
+  case class SimpleType(
+    booleanField: Boolean
+  )
+
+  object SimpleType {
+    val defaultValue: SimpleType = SimpleType(
+      booleanField = true
+    )
+  }
+
+  class SimpleTypeWithCustomEncoder(
+    val toCaseClass: SimpleType
+  ) {
+    override def equals(obj: Any): Boolean = obj match {
+      case encoder: SimpleTypeWithCustomEncoder =>
+        encoder.toCaseClass == toCaseClass
+      case _ =>
+        false
+    }
+  }
+
+  object SimpleTypeWithCustomEncoder extends Invariant[SimpleTypeWithCustomEncoder, SimpleType] {
+    override def map(in: SimpleTypeWithCustomEncoder): SimpleType = in.toCaseClass
+
+    override def contrMap(out: SimpleType): SimpleTypeWithCustomEncoder =
+      new SimpleTypeWithCustomEncoder(out)
+
+    implicit val encoder: TypedEncoder[SimpleTypeWithCustomEncoder] = InvariantEncoder(this)
+  }
+}
+
