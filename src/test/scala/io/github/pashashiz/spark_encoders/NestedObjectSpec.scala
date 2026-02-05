5e0c0bbf104caa97e9fa63234ca9fe3de0602eaa
Dan Di Spaltro <dan.dispaltro@gmail.com>
2026-02-04 20:42:35 -0800

    Add NestedObject encoder spec tests, that fail

---
+++ src/test/scala/io/github/pashashiz/spark_encoders/NestedObjectSpec.scala
@@ -0,0 +1,40 @@
+package io.github.pashashiz.spark_encoders
+import NestedObjectSpec._
+
+class NestedObjectSpec extends SparkAnyWordSpec() with TypedEncoderMatchers
+    with TypedEncoderImplicits {
+
+  "NestedObject" when {
+
+    "used with complex types" should {
+
+      "work correctly" in {
+        Foo(
+          Some("Hello"),
+          (
+            NestedObject(
+              Some("Hello!")),
+            Some(DoubleNext(1.5)))) should haveTypedEncoder[Foo]()
+      }
+
+      "work correctly without tuples" in {
+        Foo2(
+          Some("Hello"),
+          NestedTwo(
+            NestedObject(
+              Some("Hello!")),
+            DoubleNext(1.5))) should haveTypedEncoder[Foo2]()
+      }
+    }
+  }
+}
+
+object NestedObjectSpec {
+
+  case class NestedObject(value: Option[String])
+  case class DoubleNext(value: Double)
+  case class Foo(str: Option[String], nestedObject: (NestedObject, Option[DoubleNext]))
+
+  case class NestedTwo(obj: NestedObject, double: DoubleNext)
+  case class Foo2(str: Option[String], nestedObject: NestedTwo)
+}
