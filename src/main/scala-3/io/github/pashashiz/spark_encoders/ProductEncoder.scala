15b6b0d91361b5b519c23b19a1e3639da6524aab
Dan Di Spaltro <dan.dispaltro@gmail.com>
2026-02-04 22:35:40 -0800

    scala3: Make nested struct fields nullable for UpCast

--- src/main/scala-3/io/github/pashashiz/spark_encoders/ProductEncoder.scala
+++ src/main/scala-3/io/github/pashashiz/spark_encoders/ProductEncoder.scala
@@ -8,6 +8,19 @@ import io.github.pashashiz.spark_encoders.expressions.ObjectInstance
 
 import scala.reflect.ClassTag
 
+object ProductEncoder:
+  /** Recursively make all nested struct fields nullable for UpCast compatibility.
+    * Spark stores nested structs as nullable by default, so when reading back,
+    * we need the target schema to accept nullable struct fields. */
+  def makeNullable(dt: DataType): DataType = dt match {
+    case st: StructType =>
+      StructType(st.fields.map { f =>
+        f.copy(dataType = makeNullable(f.dataType), nullable = true)
+      })
+    case other => other
+  }
+
+
 class CaseObjectEncoder[A: ClassTag] extends TypedEncoder[A] {
 
   override def catalystRepr: DataType = StructType(Seq.empty)
@@ -66,7 +79,8 @@ class CaseClassEncoder[A: ClassTag](
       case (label, encoder) =>
         val paramExpr = UpCast(
           child = UnresolvedExtractValue(child = path, extraction = Literal(label)),
-          target = encoder.catalystRepr)
+          // Use makeNullable for nested structs since Spark stores them as nullable by default
+          target = ProductEncoder.makeNullable(encoder.catalystRepr))
         // we do not accept null values in Product types,
         // nullable fields should use Option instead
         AssertNotNull(encoder.fromCatalyst(paramExpr))
